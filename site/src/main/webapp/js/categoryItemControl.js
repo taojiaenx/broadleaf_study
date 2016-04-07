91fbf47c260f688d18cbbfeb92311ae6077373bc
taojiaen <taojiaen@gmail.com>
2016-04-07 22:25:11 +0800

    [change]加入显示红条

--- site/src/main/webapp/js/categoryItemControl.js
+++ site/src/main/webapp/js/categoryItemControl.js
@@ -1,11 +1,35 @@
-function categoryItemOnclick(event)
-{
-	var targetElement = evet.target;
-	for(category in $('.categoryItems')) {
-		if (category != targetElement) {
-			category.className += 'active_category';
-		} else {
-			category.className -= 'active_category';
-		}
-	}
+var readyf = function initCategories() {
+    var categories = document.getElementsByClassName('category_item');
+    if (categories == null || categories.length == 0)  return;
+
+    var nowItem = window.location.href.substr(window.location.href.lastIndexOf('/') + 1, window.location.href.length);
+
+    if (nowItem == null || nowItem.length == 0) {
+        categories[0].className += ' active_category';
+    } else {
+        categories.getElementsByName(nowItem).className += ' active_category';
+    }
+};
+
+document.ready = function (callback) {
+    ///兼容FF,Google
+    if (document.addEventListener) {
+        document.addEventListener('DOMContentLoaded', function () {
+            document.removeEventListener('DOMContentLoaded', arguments.callee, false);
+            callback();
+        }, false)
+    }
+    //兼容IE
+    else if (document.attachEvent) {
+        document.attachEvent('onreadytstatechange', function () {
+            if (document.readyState == "complete") {
+                document.detachEvent("onreadystatechange", arguments.callee);
+                callback();
+            }
+        })
+    }
+    else if (document.lastChild == document.body) {
+        callback();
+    }
 }
+document.ready = readyf;
